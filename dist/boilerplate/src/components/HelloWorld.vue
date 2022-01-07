31fbc410aa270f981039a9c1871f87c89981a258
kyejisun <olubunmi>
2022-01-07 23:52:52 +0100

    README

---
+++ dist/boilerplate/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'Helloworld',
+});
+</script>
+
+<template >
+  <div class="bg-purple-200 h-96 w-full rounded-xl p-4" component-id="48225dc5_0ae8" odin-component="true">
+    <p class="font-sans my-4 p-1 text-lg" odin-id="9a02fb8b_352b">
+      Select Component and press: alt + w to activate workbench. Edit component
+      &amp; Save Changes to source.
+    </p>
+    <div class="h-24 text-white bg-purple-900 my-4 rounded-xl" odin-id="2f64de86_b8e3">
+      <p class="font-sans text-lg" odin-id="8bffe473_d464">Edit Padding</p>
+    </div>
+    <div class="h-24 bg-white rounded-xl" odin-id="3951ff96_e054">Edit Margin</div>
+  </div>
+</template>
+
+<style scoped>
+</style>
